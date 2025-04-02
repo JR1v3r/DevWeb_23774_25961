2deb9aefee0eb89336ff2f168deccff33638faae
Joana Flores <jriverunderscore@gmail.com>
2025-04-02 11:24:00 +0100

    submissão inicial
    Início da criação do projeto
    
    Co-Authored-By: Bernardo Ferreira <102265201+MCBananna@users.noreply.github.com>

---
+++ DevWeb_23774_25961/DevWeb_23774_25961/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevWeb_23774_25961.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
