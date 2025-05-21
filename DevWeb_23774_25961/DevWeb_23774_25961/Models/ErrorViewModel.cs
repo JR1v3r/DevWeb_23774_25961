aa57c3ef42d6b2ce5f76c2e108fa342f7521fa6a
Joana Flores <jriverunderscore@gmail.com>
2025-05-21 09:24:39 +0100

    Preparação para a reformulação do projeto

--- DevWeb_23774_25961/DevWeb_23774_25961/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DevWeb_23774_25961.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- DevWeb_23774_25961/DevWeb_23774_25961/Models/TradeStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace DevWeb_23774_25961.Models;
-
-public enum TradeStatus
-{
-    Pending,
-    Accepted,
-    Denied
-}
